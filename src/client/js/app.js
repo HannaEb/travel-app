81338172a7e56b9aeec1f4be13afa139a254932b
Hanna <hannaeberhoefer@yahoo.com>
2022-01-29 14:44:37 +0000

    refactor: for rubric

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,40 +1,82 @@
-// Invoke POST request
-const performAction = e => {
-    e.preventDefault()
+// Declare API URLs
+const geonamesURL = 'http://api.geonames.org/searchJSON?q='
+const weatherbitCurrentURL = 'http://api.weatherbit.io/v2.0/current?'
+const weatherbitForecastURL = 'http://api.weatherbit.io/v2.0/forecast/daily?'
+const pixabayURL = 'https://pixabay.com/api/?'
 
-    let destination = document.getElementById('destination').value
-    let travelDate = document.getElementById('date').value
-    let todaysDate = Date.now()
-    let days = calcDays(todaysDate, travelDate)
 
-    const data = {
-        'destination': destination,
-        'days': days
-    }
+const travelDate = document.getElementById('date').value
+let geonamesData = {}
 
-    postData(data)
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+1+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+// Callback function
+const performAction = (event) => {
+    event.preventDefault();
+
+    const destination = document.getElementById('destination').value  
     
+    getData(geonamesURL+destination+'&maxRows=1&username='+geonamesKey)
+    .then(data => {
+        geonamesData = {
+            city: data.geonames[0].name,
+            country: data.geonames[0].countryName
+        }
+    })
+    .then(() => {
+        postData('http://localhost:3001/add', { 
+            geonamesData
+        })
+    })
+    .then(() => updateUI());
 }
 
-// Add eventListener to submit button
+// Event listener for action to be performed
 document.getElementById('generate').addEventListener('click', performAction);
 
-// POST request to server
-const postData = async (data) => {
-    const res = await fetch('http://localhost:3001/postData', {
+// GET request to OpenWeatherMap
+const getData = async (url) => {
+    const res = await fetch(url);
+    try {
+        const data = await res.json();
+        console.log('Client Data:')
+        console.log(data)
+        return data;
+    } catch(error) {
+        console.log('error', error)
+    }
+}
+
+// POST request
+const postData = async (url = '', data = {}) => {
+    const res = await fetch(url, {
         method: 'POST',
         credentials: 'same-origin',
         headers: {
-            'Content-Type': 'application/json',
+            'Content-Type': 'application/json'
         },
         body: JSON.stringify(data)
     })
     try {
         const newData = await res.json();
-        document.getElementById('description').innerHTML = `${newData.description}`
-        document.getElementById('temperature').innerHTML = `${newData.temperature} degrees`
-        document.getElementById('locationImage').src = `${newData.image}`
-        return newData
+        console.log('Client New Data:')
+        console.log(newData)
+        return newData;
+    } catch(error) {
+        console.log('error', error)
+    }
+}
+
+// update UI
+const updateUI = async () => {
+    const req = await fetch('http://localhost:3001/all')
+    try {
+        const allData = await req.json()
+        console.log(allData)
+        document.getElementById('city').innerHTML = allData.geonamesData.city;
+        document.getElementById('country').innerHTML = allData.geonamesData.country
     } catch(error) {
         console.log('error', error)
     }
