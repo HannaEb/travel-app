a6bdffd856f4d055dc2eeede0f8bc8b94fd7072e
Hanna <hannaeberhoefer@yahoo.com>
2022-02-01 15:15:55 +0000

    feat: update results display

--- src/client/js/updateUI.js
+++ src/client/js/updateUI.js
@@ -3,11 +3,12 @@ const updateUI = async () => {
     const req = await fetch('http://localhost:3001/all')
     try {
         const allData = await req.json()
-        document.getElementById('city').innerHTML = allData.geonamesData.city
-        document.getElementById('country').innerHTML = allData.geonamesData.country
-        document.getElementById('duration').innerHTML = allData.duration
+        document.getElementById('intro').innerHTML = 'Your upcoming trip to: '
+        document.getElementById('location').innerHTML = allData.geonamesData.city + ', ' + allData.geonamesData.country
+        document.getElementById('duration').innerHTML = allData.duration + ' nights'
+        document.getElementById('weather').innerHTML = 'Weather conditions:'
         document.getElementById('description').innerHTML = allData.weatherbitData.description
-        document.getElementById('temperature').innerHTML = allData.weatherbitData.temperature
+        document.getElementById('temperature').innerHTML = allData.weatherbitData.temperature + ' degrees'
         document.getElementById('locationImage').src = allData.pixabayData.image
     } catch(error) {
         console.log('error', error)
