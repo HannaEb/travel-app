41635619551a680eb4d11cc1481b8fad5275c1b2
Hanna <hannaeberhoefer@yahoo.com>
2022-03-14 20:46:52 +0000

    style: formatting

--- src/client/js/calculateDays.js
+++ src/client/js/calculateDays.js
@@ -1,10 +1,10 @@
 // Calculate days
 const calcDays = (start, end) => {
-    const startDate = new Date(start)
-    const endDate = new Date(end)
+  const startDate = new Date(start);
+  const endDate = new Date(end);
 
-    const days = Math.round((endDate - startDate) / (1000 * 60 * 60 * 24) + 1)
-    return days
-}
+  const days = Math.round((endDate - startDate) / (1000 * 60 * 60 * 24) + 1);
+  return days;
+};
 
-export { calcDays }
+export { calcDays };
--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,112 +1,137 @@
 // Declare API URLs
-const geonamesURL = 'http://api.geonames.org/searchJSON?q='
-const weatherbitCurrentURL = 'http://api.weatherbit.io/v2.0/current?'
-const weatherbitForecastURL = 'http://api.weatherbit.io/v2.0/forecast/daily?'
-const pixabayURL = 'https://pixabay.com/api/?'
+const geonamesURL = "http://api.geonames.org/searchJSON?q=";
+const weatherbitCurrentURL = "http://api.weatherbit.io/v2.0/current?";
+const weatherbitForecastURL = "http://api.weatherbit.io/v2.0/forecast/daily?";
+const pixabayURL = "https://pixabay.com/api/?";
 
 // Declare variables
-let apiKeys = {}
-let geonamesData = {}
-let weatherbitData = {}
-let pixabayData = {}
+let apiKeys = {};
+let geonamesData = {};
+let weatherbitData = {};
+let pixabayData = {};
 
 // Callback function
 const handleSubmit = (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const destination = document.getElementById('destination').value 
-    const startDate = document.getElementById('startDate').value
-    const endDate = document.getElementById('endDate').value
-    let duration = Client.calcDays(startDate, endDate) - 1
-    let days = Client.calcDays(Date.now(), startDate)
-    let day 
+  const destination = document.getElementById("destination").value;
+  const startDate = document.getElementById("startDate").value;
+  const endDate = document.getElementById("endDate").value;
+  let duration = Client.calcDays(startDate, endDate) - 1;
+  let days = Client.calcDays(Date.now(), startDate);
+  let day;
 
-    if (days <= 7 || days > 16) {
-        day = 0
-    } else {
-        day = days
-    }
+  if (days <= 7 || days > 16) {
+    day = 0;
+  } else {
+    day = days;
+  }
 
-    Client.retrieveApiKeys()
+  Client.retrieveApiKeys()
     .then((data) => {
-        apiKeys = data
-        let geonamesKey = apiKeys['geonamesKey']
-        return getData(geonamesURL+destination+'&maxRows=1&username='+geonamesKey)
+      apiKeys = data;
+      let geonamesKey = apiKeys["geonamesKey"];
+      return getData(
+        geonamesURL + destination + "&maxRows=1&username=" + geonamesKey
+      );
     })
-    .then(data => {
-        geonamesData = {
-            city: data.geonames[0].name,
-            country: data.geonames[0].countryName
-        }
+    .then((data) => {
+      geonamesData = {
+        city: data.geonames[0].name,
+        country: data.geonames[0].countryName,
+      };
 
-        let weatherbitKey = apiKeys['weatherbitKey']
+      let weatherbitKey = apiKeys["weatherbitKey"];
 
-        if (days <= 7) {
-            return getData(weatherbitCurrentURL+'lat='+data.geonames[0].lat+'&lon='+data.geonames[0].lng+'&key='+weatherbitKey)
-        } else {
-            return getData(weatherbitForecastURL+'lat='+data.geonames[0].lat+'&lon='+data.geonames[0].lng+'&key='+weatherbitKey)
-        }
+      if (days <= 7) {
+        return getData(
+          weatherbitCurrentURL +
+            "lat=" +
+            data.geonames[0].lat +
+            "&lon=" +
+            data.geonames[0].lng +
+            "&key=" +
+            weatherbitKey
+        );
+      } else {
+        return getData(
+          weatherbitForecastURL +
+            "lat=" +
+            data.geonames[0].lat +
+            "&lon=" +
+            data.geonames[0].lng +
+            "&key=" +
+            weatherbitKey
+        );
+      }
     })
-    .then(data => {
-        weatherbitData = {
-            description: data.data[day].weather.description,
-            temperature: data.data[day].temp
-        }
+    .then((data) => {
+      weatherbitData = {
+        description: data.data[day].weather.description,
+        temperature: data.data[day].temp,
+      };
 
-        let pixabayKey = apiKeys['pixabayKey']
+      let pixabayKey = apiKeys["pixabayKey"];
 
-        return getData(pixabayURL+'key='+pixabayKey+'&q='+destination+'&image-type=photo&orientation=horizontal')
+      return getData(
+        pixabayURL +
+          "key=" +
+          pixabayKey +
+          "&q=" +
+          destination +
+          "&image-type=photo&orientation=horizontal"
+      );
     })
-    .then(data => {
-        pixabayData = {
-            imageOne: data.hits[0].webformatURL,
-            imageTwo: data.hits[1].webformatURL,
-            imageThree: data.hits[2].webformatURL
-        }
+    .then((data) => {
+      pixabayData = {
+        imageOne: data.hits[0].webformatURL,
+        imageTwo: data.hits[1].webformatURL,
+        imageThree: data.hits[2].webformatURL,
+      };
     })
     .then(() => {
-        postData('http://localhost:3001/add', { 
-            duration,
-            geonamesData,
-            weatherbitData,
-            pixabayData
-        })
+      postData("http://localhost:3001/add", {
+        duration,
+        geonamesData,
+        weatherbitData,
+        pixabayData,
+      });
     })
-    .then(() => Client.updateUI())
-}
+    .then(() => Client.updateUI());
+};
 
 // Event listener for action to be performed
-document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('generate').addEventListener('click', handleSubmit)
-})
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("generate").addEventListener("click", handleSubmit);
+});
 
 // GET request
 const getData = async (url) => {
-    const res = await fetch(url);
-    try {
-        const data = await res.json();
-        return data;
-    } catch(error) {
-        console.log('error', error)
-    }
-}
+  const res = await fetch(url);
+  try {
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
 
 // POST request
-const postData = async (url = '', data = {}) => {
-    const res = await fetch(url, {
-        method: 'POST',
-        credentials: 'same-origin',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    })
-    try {
-        const newData = await res.json();
-        return newData;
-    } catch(error) {
-        console.log('error', error)
-    }
-}
+const postData = async (url = "", data = {}) => {
+  const res = await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  try {
+    const newData = await res.json();
+    return newData;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
 
-export { handleSubmit }
+export { handleSubmit };
