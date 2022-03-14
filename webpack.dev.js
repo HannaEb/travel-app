28349c91ccfb95cac7c9f5dd6d2cbbc95b46ab0b
Hanna <hannaeberhoefer@yahoo.com>
2022-03-14 16:51:05 +0000

    feat: update logo/favicon

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,66 +1,66 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 // const WorkboxPlugin = require('workbox-webpack-plugin')
 
 module.exports = {
-    entry: './src/client/index.js',
-    output: {
-        libraryTarget: 'var',
-        library: 'Client'
-    },
-    mode: 'development',
-    devtool: 'source-map',
-    stats: 'verbose',
-    devServer: {
-        port: 3000
-    },
-    module: {
-        rules: [
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: 'babel-loader'
+  entry: "./src/client/index.js",
+  output: {
+    libraryTarget: "var",
+    library: "Client",
+  },
+  mode: "development",
+  devtool: "source-map",
+  stats: "verbose",
+  devServer: {
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: "/.js$/",
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[contenthash].[ext]",
+              outputPath: "static/img",
+              esModule: false,
             },
-            {
-                test: /\.scss$/,
-                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
-            },
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[contenthash].[ext]',
-                            outputPath: 'static/img',
-                            esModule: false
-                        }
-                    },
-                ],
-            },
-            {
-                test: /\.html$/i,
-                loader: "html-loader",
-            },
-        ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: './src/client/views/index.html',
-            filename: './index.html',
-            favicon: './src/client/media/favicon.png',
-        }),
-        // new WorkboxPlugin.GenerateSW(),
-        new CleanWebpackPlugin({
-            // Simulate the removal of files
-            dry: true,
-            // Write Logs to Console
-            verbose: true,
-            // Automatically remove all unused webpack assets on rebuild
-            cleanStaleWebpackAssets: true,
-            protectWebpackAssets: false
-        })
-    ]
-}
+          },
+        ],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html",
+      favicon: "./src/client/media/favicon.png",
+    }),
+    // new WorkboxPlugin.GenerateSW(),
+    new CleanWebpackPlugin({
+      // Simulate the removal of files
+      dry: true,
+      // Write Logs to Console
+      verbose: true,
+      // Automatically remove all unused webpack assets on rebuild
+      cleanStaleWebpackAssets: true,
+      protectWebpackAssets: false,
+    }),
+  ],
+};
