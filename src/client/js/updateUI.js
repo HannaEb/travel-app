121b2020c2b5c9f8a4cd15cc3e4b48a994b4e120
Hanna <hannaeberhoefer@yahoo.com>
2022-03-15 14:10:48 +0000

    feat: link to tripadvisor

--- src/client/js/updateUI.js
+++ src/client/js/updateUI.js
@@ -1,20 +1,38 @@
 // Update UI
 const updateUI = async () => {
-    const req = await fetch('http://localhost:3001/all')
-    try {
-        const allData = await req.json()
-        document.getElementById('intro').innerHTML = 'Your upcoming trip to: '
-        document.getElementById('location').innerHTML = allData.geonamesData.city + ', ' + allData.geonamesData.country
-        document.getElementById('duration').innerHTML = allData.duration + ' nights'
-        document.getElementById('weather').innerHTML = 'Weather conditions:'
-        document.getElementById('description').innerHTML = allData.weatherbitData.description
-        document.getElementById('temperature').innerHTML = allData.weatherbitData.temperature + ' degrees'
-        document.getElementById('image-1').src = allData.pixabayData.imageOne
-        document.getElementById('image-2').src = allData.pixabayData.imageTwo
-        document.getElementById('image-3').src = allData.pixabayData.imageThree
-    } catch(error) {
-        console.log('error', error)
-    }
-}
+  const req = await fetch("http://localhost:3001/all");
+  try {
+    const allData = await req.json();
+    document.getElementById("intro").innerHTML = "Your upcoming trip to ";
+    document.getElementById(
+      "location"
+    ).innerHTML = `${allData.geonamesData.city}, ${allData.geonamesData.country}`;
+    document.getElementById(
+      "duration"
+    ).innerHTML = `For ${allData.duration} nights`;
+    document.getElementById(
+      "date"
+    ).innerHTML = `${allData.weatherbitData.date}`;
+    document.getElementById(
+      "weather-icon"
+    ).innerHTML = `${allData.weatherbitData.icon}`;
+    document.getElementById(
+      "description"
+    ).innerHTML = `${allData.weatherbitData.description}`;
+    document.getElementById(
+      "temperature"
+    ).innerHTML = `${allData.weatherbitData.temperature} degrees`;
+    document.getElementById("image-1").src = `${allData.pixabayData.imageOne}`;
+    document.getElementById("image-2").src = `${allData.pixabayData.imageTwo}`;
+    document.getElementById(
+      "image-3"
+    ).src = `${allData.pixabayData.imageThree}`;
+    document.getElementById(
+      "btn-tripadvisor"
+    ).href = `https://www.tripadvisor.com/Search?q=${allData.geonamesData.city}`;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
 
-export { updateUI }
+export { updateUI };
