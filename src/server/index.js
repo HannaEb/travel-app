2174c7d1905bdb0113db1193135cd924234d28e9
Hanna <hannaeberhoefer@yahoo.com>
2022-07-09 10:15:14 +0100

    feat: change port

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,8 @@
 // Require app
-const app = require('./server')
+const app = require("./server");
 
 // Set up server
-const port = 3001
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+const port = 3001;
+app.listen(process.env.PORT || port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
