42b05c5f1aeca46d202cb9a0bda14f33e9a468d2
Hanna <hannaeberhoefer@yahoo.com>
2022-10-06 12:47:32 +0100

    refactor: calcNights

--- src/client/js/calculateNights.js
+++ src/client/js/calculateNights.js
@@ -3,8 +3,8 @@ const calcNights = (start, end) => {
   const startDate = new Date(start);
   const endDate = new Date(end);
 
-  const days = Math.round((endDate - startDate) / (1000 * 60 * 60 * 24));
-  return days;
+  const nights = Math.round((endDate - startDate) / (1000 * 60 * 60 * 24));
+  return nights;
 };
 
 export { calcNights };
